9e1860b5a2907cf6c3f28896fc3460a66c96500d
Robert Taddeo <robtaddeo@gmail.com>
2019-12-27 14:50:10 -0500

    Fixing React Client

---
+++ go-caddy-client/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+// MUI Stuff
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+// Pages
+import home from './pages/home';
+// import commerce from './pages/features/commerce';
+// import support from './pages/features/support';
+// import websites from './pages/features/websites';
+
+// Components
+import Navbar from './components/Navbar';
+
+const theme = createMuiTheme({
+	typography: {
+		fontFamily: [ 'Lato', 'Roboto', '"Helvetica Neue"', 'Arial', 'sans-serif' ].join(',')
+	}
+});
+
+function App() {
+	return (
+		<ThemeProvider theme={theme}>
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route exact path="/" component={home} />
+				</Switch>
+			</Router>
+		</ThemeProvider>
+	);
+}
+
+export default App;
